056743ffdd478a5d8e062db9231bc2af17f98e59
Sony <Sony@DESKTOP-IR8M8ES>
2024-09-17 00:06:23 +0530

    1st change

---
+++ src/java8/ListOperation.java
@@ -0,0 +1,14 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListOperation {
+
+	public static void main(String[] args) {
+		String []s= {"ram","shyam","sita","gita"};
+		List<String> asList = Arrays.asList(s);
+		System.out.println(asList);
+	}
+
+}
